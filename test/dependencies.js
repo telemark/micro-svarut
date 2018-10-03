53e87bfcc78f36fee1bb3b3116b8ea99a6dcda47
Jonas Enge <jonas.enge@gmail.com>
2018-10-03 11:45:54 +0200

    v3 support v10 of forsendelseservice

--- test/dependencies.js
+++ test/dependencies.js
@@ -2,21 +2,21 @@ const test = require('ava')
 const pkg = require('../package.json')
 const devDependencies = pkg.devDependencies || {}
 const dependencies = pkg.dependencies || {}
-const dropModules = ['micro-dev', 'nsp']
-const isDropped = (module) => !dropModules.includes(module)
+const dropModules = ['micro-dev']
+const isDropped = module => !dropModules.includes(module)
 
 test('basic check', t => {
   t.true(true, 'ava works ok')
 })
 
-Object.keys(devDependencies).filter(isDropped).forEach((dependency) => {
+Object.keys(devDependencies).filter(isDropped).forEach(dependency => {
   test(`${dependency} loads ok`, t => {
     const module = require(dependency)
     t.truthy(module)
   })
 })
 
-Object.keys(dependencies).forEach((dependency) => {
+Object.keys(dependencies).forEach(dependency => {
   test(`${dependency} loads ok`, t => {
     const module = require(dependency)
     t.truthy(module)
